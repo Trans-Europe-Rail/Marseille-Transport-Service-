d95ca1a1e841b57f33922190c16cf13ebe6315d9
Trans-Europe-Rail <jeuxvideo778@gmail.com>
2026-07-25 13:11:46 +0200

    Refactor app.run() for production settings
    Removed debug mode from app.run() and updated to use environment variable for port.

--- app.py
+++ app.py
@@ -151,13 +151,10 @@ def api_data():
 def lignes():
     return render_template('lignes.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
 @app.errorhandler(404)
 def not_found(e):
     return render_template("404.html"), 404
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
-    
+        
