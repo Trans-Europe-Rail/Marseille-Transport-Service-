0d5a132ec1e15aa27fc10592af53aa86b9301f4e
Trans-Europe-Rail <jeuxvideo778@gmail.com>
2026-07-25 12:44:10 +0200

    Change default port to 10000 in app.py

--- app.py
+++ app.py
@@ -159,5 +159,6 @@ def not_found(e):
     return render_template("404.html"), 404
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
     
